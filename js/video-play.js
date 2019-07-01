df3a049ca43d5819adf7c9ae222930da121bcf34
alex-obr@bitbucket.org <obram22@gmail.com>
2019-07-01 16:27:50 +0300

    add autoplay

--- js/video-play.js
+++ js/video-play.js
@@ -2,18 +2,27 @@ function playVideo(id, play) {
     var element = document.getElementById(id);
     var playBtn = document.getElementById(play);
 
-    console.log(element);
-    console.log(playBtn);
-
     playBtn.addEventListener('click', function(e) {
+    	openFullscreen(element);
         element.play();
         e.currentTarget.style.display = "none";
         
     })
 }
 
+
+function openFullscreen(element) {
+  if (element.requestFullscreen) {
+    element.requestFullscreen();
+  } else if (element.mozRequestFullScreen) { /* Firefox */
+    element.mozRequestFullScreen();
+  } else if (element.webkitRequestFullscreen) { /* Chrome, Safari & Opera */
+    element.webkitRequestFullscreen();
+  } else if (element.msRequestFullscreen) { /* IE/Edge */
+    element.msRequestFullscreen();
+  }
+}
+
 $(function() {
 	playVideo('video-mobile', 'js-mobile-play');
-})
-
-    
+});
