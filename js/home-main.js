f09f47dfcd8d577279ca31e11dbeb43d176741ae
alex-obr@bitbucket.org <obram22@gmail.com>
2019-07-01 13:20:32 +0300

    bugfix

--- js/home-main.js
+++ js/home-main.js
@@ -84,3 +84,24 @@ $(window).on('resize scroll', function() {
         $('#section-2').find('.tooltip').removeClass('active');
     }
 });
+
+
+
+/*Autoplay video JQuery when video is in center of my screen*/
+$(window).scroll(function(e)
+  {
+    var offsetRange = $(window).height() / 3,
+        offsetTop = $(window).scrollTop() + offsetRange + $("#header").outerHeight(true),
+        offsetBottom = offsetTop + offsetRange;
+
+    $("#video").each(function () { 
+      var y1 = $(this).offset().top;
+      var y2 = offsetTop;
+      if (y1 + $(this).outerHeight(true) < y2 || y1 > offsetBottom) {
+        this.pause(); 
+      } else {
+        this.play();
+      }
+    });
+});
+
